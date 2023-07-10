a06cb6590d076eeaec9a4326adcbbbc29c9d371b
Ivo <jack@Ivos-MBP.lan>
2023-07-10 13:19:22 +0300

    Adding team member

--- src/app/addTeamMembers/page.tsx
+++ src/app/addTeamMembers/page.tsx
@@ -1,152 +1,160 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import React,{useState} from 'react'
-import AddTeamMember from './teamMember/page';
-import { useProfileContext } from '@/Context/ProfileStore';
-import { useWalletContext } from '@/Context/WalletStore';
-import { usePhalaContractContext } from '@/Context/PhalaContractApiStore';
-import { AccountId, MemberRole, UserRole } from "@/lib/PhalaContract/Types/types";
-import { createApplicantProfile } from '@/lib/PhalaContract/Txn/createProfile';
-
+import Link from "next/link";
+import React, { useState } from "react";
+import AddTeamMember from "./teamMember/page";
+import { useProfileContext } from "@/Context/ProfileStore";
+import { useWalletContext } from "@/Context/WalletStore";
+import { usePhalaContractContext } from "@/Context/PhalaContractApiStore";
+import {
+  AccountId,
+  MemberRole,
+  UserRole,
+} from "@/lib/PhalaContract/Types/types";
+import { createApplicantProfile } from "@/lib/PhalaContract/Txn/createProfile";
+import TeamMemberBasic from "./teamMember/TeamMeberBasic";
 
 export interface Member {
-    acc: AccountId,
-    role: MemberRole
+  acc: AccountId;
+  role: MemberRole;
 }
 
 function AddTeamMemberPage() {
-    //Context
-    const {profileData, setProfile,creationStatus,setCreationStatus} = useProfileContext();
-    const {account, signer} = useWalletContext();
-    const {contractApi,cache} =  usePhalaContractContext();
+  //Context
+  const { profileData, setProfile, creationStatus, setCreationStatus } =
+    useProfileContext();
+  const { account, signer } = useWalletContext();
+  const { contractApi, cache } = usePhalaContractContext();
 
-    const defaultMember: Member = {
-        acc: "",
-        role: MemberRole.regular
-    }
-    const [memberCount, setMemberCount] = useState<number>(1);
-    const [membersNRole, setMembersNRole] = useState<Array<[AccountId, MemberRole]>>([]);
-    const [members, setMembers] = useState<Member>(defaultMember)
+  const defaultMember: Member = {
+    acc: "",
+    role: MemberRole.regular,
+  };
+  const [memberCount, setMemberCount] = useState<number>(1);
+  const [membersNRole, setMembersNRole] = useState<
+    Array<[AccountId, MemberRole]>
+  >([]);
+  const [members, setMembers] = useState<Member>(defaultMember);
 
-    const addMember = (v: Member) => {
-        setMembers({ ...members, ...v })
-      }
-    
-    
-    const addTeamMember = () => {
-        const memberData: [AccountId, MemberRole] = [members.acc, members.role];
-        setMembersNRole((prevData) => [...prevData, memberData])
-        setMembers(defaultMember)
-        setMemberCount(memberCount + 1);
-      };
-    
-      const saveNDone = async () => {
-        //@ts-ignore
-        setProfile({ teamMembers: membersNRole })
-        await createProfile()
-      }
+  const addMember = (v: Member) => {
+    setMembers({ ...members, ...v });
+  };
 
-      // Contract Call for Crreating Profile
+  const addTeamMember = () => {
+    const memberData: [AccountId, MemberRole] = [members.acc, members.role];
+    setMembersNRole((prevData) => [...prevData, memberData]);
+    setMembers(defaultMember);
+    setMemberCount(memberCount + 1);
+  };
+
+//   const addNewMember = (newMember: { acc: AccountId; role: MemberRole }) => {};
+
+  const saveNDone = async () => {
+    //@ts-ignore
+    setProfile({ teamMembers: membersNRole });
+    await createProfile();
+  };
+
+  // Contract Call for Crreating Profile
   const createProfile = async () => {
-    const profileCreationStatus =(v:boolean)=>{
-        setCreationStatus(v)
+    const profileCreationStatus = (v: boolean) => {
+      setCreationStatus(v);
+    };
+
+    const { teamType, userType } = profileData;
+    if (account && signer && cache && contractApi) {
+      //1. (Individual && Applicant)
+      if (teamType === "Individual" && userType === "Applicant") {
+        await createApplicantProfile(
+          //utill fn
+          profileCreationStatus,
+          //---------
+          account,
+          signer,
+          cache,
+          contractApi,
+          //Params
+          profileData.teamName,
+          account.address,
+          profileData.description,
+          profileData.allowedAccounts,
+          [],
+          //ProfileCtx.profileData.projectType, // Work on this
+          profileData.teamMembers,
+          [],
+          UserRole.individual
+        );
+      }
+      //2. (Individual && Grant Issuer)
+      if (teamType === "Individual" && userType === "Grant Issuer") {
+      }
+      //3. (Organization && Applicant)
+      if (teamType === "Organization" && userType === "Applicant") {
+      }
+      //4. (Organization && Grant Issuer)
+      if (teamType === "Organization" && userType === "Grant Issuer") {
+      }
     }
-
-    const { teamType, userType } = profileData
-    if(account && signer && cache && contractApi){
-
-       //1. (Individual && Applicant)
-        if (teamType === "Individual" && userType === "Applicant") {
-          await createApplicantProfile(
-            //utill fn
-             profileCreationStatus,
-            //---------
-              account,
-              signer,
-              cache,
-              contractApi,
-              //Params
-              profileData.teamName,
-              account.address,
-              profileData.description,
-              profileData.allowedAccounts,
-              [],
-              //ProfileCtx.profileData.projectType, // Work on this
-              profileData.teamMembers,
-              [],
-              UserRole.individual
-            )
-        }
-        //2. (Individual && Grant Issuer)
-        if (teamType === "Individual" && userType === "Grant Issuer") {
-
-        }
-        //3. (Organization && Applicant)
-        if (teamType === "Organization" && userType === "Applicant") {
-
-        }
-        //4. (Organization && Grant Issuer)
-        if (teamType === "Organization" && userType === "Grant Issuer") {
-
-        }
-          
-    }
- }
-
+  };
 
   return (
-    <div className="font-space-grotesk grid h-screen place-items-center relative mb-20">
-        <div className="absolute insext-x-0 top-20 max-w-xl">
-            <h1 className="text-4xl font-medium mb-4">Add team members</h1>
-            <p className="paragraph mb-12">
-                Add your team and send them wallet invitations. Remember to add your
-                own personal wallet if the one you logged in with is a multi sig or
-                belongs to an organisation. 
-            </p>
-            <p>Member Wallet address</p>
-            <ul>
-                {
-                    [...Array(memberCount)].map((e,i)=> (
-                        <li key={i}>
-                            <AddTeamMember member={membersNRole} key={i - 1} setMember={addMember}/>
-                        </li>
-                    ))
-                }
-            </ul>
-            
-            <button
-                 className="mt-2 border border-black w-full py-3 text-lg"
-            >
-                Add More
-            </button>
-            <button
-                className="mt-4 border bg-black text-white w-full py-3 text-lg"
-                onClick={() => saveNDone()}
-            >
-                Create Profile
-            </button>
-             {/* If the Profile is created render the below UI */}
-            {
-                creationStatus &&
-               <button
-               className="mt-2 border border-black w-full py-3 text-lg"
-               >
-                <Link href="/profile/about">
-                    Go to Profile
-                </Link>
-               </button>
-            }
-            <button
-                className="mt-4 border bg-gray-400 text-white w-full py-3 text-lg"
-            >
-                <Link href="/createTeamProfile">
-                    Back
-                </Link>
-            </button>
+    <div className="grid h-screen place-items-center  bg-[url('/background/grain-cover.png')]  text-white">
+      <div className="w-[40rem] p-5 border border-2 border-white backdrop-blur-md rounded-lg">
+        <h1 className="text-4xl font-medium mb-4">Add team members</h1>
+        <p className="paragraph mb-12">
+          Add your team and send them wallet invitations. Remember to add your
+          own personal wallet if the one you logged in with is a multi sig or
+          belongs to an organisation.
+        </p>
+        <p>Member Wallet address:</p>
+
+        {/* <ul>
+          {[...Array(memberCount)].map((e, i) => (
+            <li key={i}>
+              <AddTeamMember
+                member={membersNRole}
+                key={i - 1}
+                setMember={addMember}
+              />
+            </li>
+          ))}
+        </ul> */}
+        
+
+        <div className="mt-4">
+            {membersNRole.map( (member) => {
+                // return <TeamMemberBasic account={member}/>
+                return <p key="">{member[0]}</p>
+            })}
         </div>
+       
+
+        <AddTeamMember member={membersNRole} setMember={addTeamMember} />
+
+        <div
+          className="mt-10
+            w-full
+            flex flex-col gap-4"
+        >
+          <button
+            className="rounded-full py-2.5 md:py-3 bg-ordum-purple  font-semibold shadow-md shadow-md hover:shadow-2xl"
+            onClick={() => saveNDone()}
+          >
+            Create Profile
+          </button>
+          {/* If the Profile is created render the below UI */}
+          {creationStatus && (
+            <button className="rounded-full py-2.5 md:py-3 bg-ordum-blue font-semibold shadow-md shadow-xl hover:shadow-2xl">
+              <Link href="/profile/about">Go to Profile</Link>
+            </button>
+          )}
+          <button className="rounded-full py-2.5 md:py-3 bg-ordum-blue font-semibold shadow-md shadow-xl hover:shadow-2xl">
+            <Link href="/createTeamProfile">Back</Link>
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AddTeamMemberPage
+export default AddTeamMemberPage;
--- src/app/addTeamMembers/teamMember/page.tsx
+++ src/app/addTeamMembers/teamMember/page.tsx
@@ -5,32 +5,38 @@ import React from "react";
 
 function AddTeamMember({ member, setMember, key }: any) {
   return (
-    <div className="flex">
-      <input
-        type="text"
-        placeholder="Confirming team member identity+send invite "
-        className="
+    <div className="flex flex-col w-full">
+      <div className="flex">
+        <input
+          type="text"
+          placeholder="Confirming team member identity+send invite "
+          className="
          w-screen
          mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-  focus:outline-none focus:border-sky-500
+  focus:outline-none focus:border-sky-500 text-gray-500
            "
-        value={member[key]?.acc}
-        onChange={(e) => setMember({ acc: e.target.value })}
-      />
-      <select
-        className="
+          // value={member[key]?.acc}
+          // onChange={(e) => setMember({ acc: e.target.value })}
+        />
+        <select
+          className="
            ml-2 
             mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-     focus:outline-none focus:border-sky-500
+     focus:outline-none focus:border-sky-500 text-gray-500
               "
-        onChange={(e) => setMember({ role: e.target.value })}
-      >
-        <option value="" disabled selected hidden>
-          Role
-        </option>
-        <option value={MemberRole.admin}>{MemberRole.admin}</option>
-        <option value={MemberRole.regular}>{MemberRole.regular}</option>
-      </select>
+          onChange={(e) => setMember({ role: e.target.value })}
+        >
+          <option value="" disabled selected hidden>
+            Role
+          </option>
+          <option value={MemberRole.admin}>{MemberRole.admin}</option>
+          <option value={MemberRole.regular}>{MemberRole.regular}</option>
+        </select>
+      </div>
+      <button className=" mt-5 rounded-full py-2 md:py-2.5 bg-ordum-blue  font-semibold shadow-md shadow-xl hover:shadow-2xl"
+      onClick={ () => {setMember()}}>
+        + Add Team Member
+      </button>
     </div>
   );
 }
