eb209065f629fdfc96a82851c127c9a09e2972e3
Ivo <jack@Ivos-MBP.lan>
2023-07-27 04:06:18 +0300

    changes

---
+++ src/app/explore/kusamatreasury/bigspender/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import GrantPage from "@/Components/Grants/Kusama";
+import Layout from "@/Components/ui/Layout";
+
+const BigSpender = () => {
+  return (
+    <Layout title={"Explore | Kusama Treasury | Big Spender"} grant>
+      Big Spender
+    </Layout>
+  );
+};
+
+export default BigSpender;
